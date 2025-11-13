9807ceda52da5038401adda57db22d6f6d72ea15
b1tsmaster <joviwong1999@gmail.com>
2025-11-13 19:59:02 +0800

    add react router

---
+++ src/Home.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+export default function Home() {
+    const [count, setCount] = useState(0);
+
+  return (
+    <div className="max-w-screen min-h-screen bg-gray-50 flex flex-col items-center justify-center p-6">
+      <div className="flex items-center gap-6 mb-6">
+        <a
+          href="https://vite.dev"
+          target="_blank"
+          rel="noreferrer"
+          className="inline-block"
+        >
+          <img
+            src={viteLogo}
+            className="h-20 w-20 animate-spin"
+            alt="Vite logo"
+          />
+        </a>
+        <a
+          href="https://react.dev"
+          target="_blank"
+          rel="noreferrer"
+          className="inline-block"
+        >
+          <img src={reactLogo} className="h-20 w-20" alt="React logo" />
+        </a>
+      </div>
+
+      <h1 className="text-4xl font-extrabold mb-4">Vite + React</h1>
+
+      <div className="bg-white shadow-md rounded-lg p-6 mb-4 w-full max-w-md text-center">
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-md"
+        >
+          count is {count}
+        </button>
+
+        <p className="mt-4 text-sm text-gray-600">
+          Edit <code className="bg-gray-100 px-1 rounded">src/App.tsx</code> and
+          save to test HMR
+        </p>
+      </div>
+
+      <p className="text-sm text-gray-500">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
